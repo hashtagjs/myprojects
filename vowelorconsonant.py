c0e8d83873d54ee4ffe376eedfa7cf7665317e4c
hashtagjs <52577257+hashtagjs@users.noreply.github.com>
2019-07-15 16:37:17 +0530

    Update vowelorconsonant.py

--- vowelorconsonant.py
+++ vowelorconsonant.py
@@ -2,7 +2,7 @@ vowels = ['a','e','i','o','u','A','E','I','O','U']
 n = raw_input()
 if (n.isalpha()):
     if (n in vowels):
-        print("vowel")
+        print("Vowel")
     else:
         print("Consonant")
 else:
