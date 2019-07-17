843f43c0a12c9eeef7590be574bd5eed482189e5
hashtagjs <52577257+hashtagjs@users.noreply.github.com>
2019-07-17 19:50:43 +0530

    Create patternalphabet.py

---
+++ patternalphabet.py
@@ -0,0 +1,23 @@
+i = input()
+sum = 0
+x = i - 1
+revsum = i - 1
+space = i - 1k
+revspace = 1
+for z in range(0,i):
+    j = 65 + z
+    c = chr(j)
+    sum+=1
+    bs = sum*c
+    print " "* space,
+    print bs
+    space = space - 1
+while (x>=0):
+    t = 64 + x
+    d = chr(t)
+    bs2 = revsum * d
+    print " "*revspace,
+    print bs2
+    revspace += 1
+    revsum = revsum - 1
+    x = x - 1
